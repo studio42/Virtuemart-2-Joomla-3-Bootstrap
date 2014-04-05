46f9fedb5c2efe44a3cc76cfa4c6c4a845958678
studio 42 france <studio.france@gmail.com>
2014-04-05 22:56:51 +0200

    icon update

--- administrator/components/com_virtuemart/install/install_essential_data.sql
+++ administrator/components/com_virtuemart/install/install_essential_data.sql
@@ -9,35 +9,36 @@
 --
 -- Dumping data for table `#__virtuemart_adminmenuentries`
 --
-INSERT INTO `#__virtuemart_adminmenuentries` (`id`, `module_id`, `parent_id`, `name`, `link`, `depends`, `icon_class`, `ordering`, `published`, `tooltip`, `view`, `task`) VALUES
-(null , 1, 0, 'COM_VIRTUEMART_CATEGORY_S', '', '', 'vmicon vmicon-16-folder_camera', 1, 1, '', 'category', ''),
-(null, 1, 0, 'COM_VIRTUEMART_PRODUCT_S', '', '', 'vmicon vmicon-16-camera', 2, 1, '', 'product', ''),
-(null, 1, 0, 'COM_VIRTUEMART_PRODUCT_CUSTOM_FIELD_S', '', '', 'vmicon vmicon-16-document_move', 5, 1, '', 'custom', ''),
-(null, 1, 0, 'COM_VIRTUEMART_PRODUCT_INVENTORY', '', '', 'vmicon vmicon-16-price_watch', 7, 1, '', 'inventory', ''),
-(null, 1, 0, 'COM_VIRTUEMART_CALC_S', '', '', 'vmicon vmicon-16-calculator', 8, 1, '', 'calc', ''),
-(null, 1, 0, 'COM_VIRTUEMART_REVIEW_RATE_S', '', '', 'vmicon vmicon-16-comments', 9, 1, '', 'ratings', ''),
-(null, 2, 0, 'COM_VIRTUEMART_ORDER_S', '', '', 'vmicon vmicon-16-page_white_stack', 1, 1, '', 'orders', ''),
-(null, 2, 0, 'COM_VIRTUEMART_COUPON_S', '', '', 'vmicon vmicon-16-shopping', 10, 1, '', 'coupon', ''),
-(null, 2, 0, 'COM_VIRTUEMART_REPORT', '', '', 'vmicon vmicon-16-to_do_list_cheked_1', 3, 1, '', 'report',''),
-(null, 2, 0, 'COM_VIRTUEMART_USER_S', '', '', 'vmicon vmicon-16-user', 4, 1, '', 'user', ''),
-(null, 2, 0, 'COM_VIRTUEMART_SHOPPERGROUP_S', '', '', 'vmicon vmicon-16-user-group', 5, 1, '', 'shoppergroup', ''),
-(null, 3, 0, 'COM_VIRTUEMART_MANUFACTURER_S', '', '', 'vmicon vmicon-16-wrench_orange', 1, 1, '', 'manufacturer', ''),
-(null, 3, 0, 'COM_VIRTUEMART_MANUFACTURER_CATEGORY_S', '', '', 'vmicon vmicon-16-folder_wrench', 2, 1, '', 'manufacturercategories', ''),
-(null, 4, 0, 'COM_VIRTUEMART_STORE', '', '', 'vmicon vmicon-16-reseller_account_template', 1, 1, '', 'user', 'editshop'),
-(null, 4, 0, 'COM_VIRTUEMART_MEDIA_S', '', '', 'vmicon vmicon-16-pictures', 2, 1, '', 'media', ''),
-(null, 4, 0, 'COM_VIRTUEMART_SHIPMENTMETHOD_S', '', '', 'vmicon vmicon-16-lorry', 3, 1, '', 'shipmentmethod', ''),
-(null, 4, 0, 'COM_VIRTUEMART_PAYMENTMETHOD_S', '', '', 'vmicon vmicon-16-creditcards', 4, 1, '', 'paymentmethod', ''),
-(null, 5, 0, 'COM_VIRTUEMART_CONFIGURATION', '', '', 'vmicon vmicon-16-config', 1, 1, '', 'config', ''),
-(null, 5, 0, 'COM_VIRTUEMART_USERFIELD_S', '', '', 'vmicon vmicon-16-participation_rate', 2, 1, '', 'userfields', ''),
-(null, 5, 0, 'COM_VIRTUEMART_ORDERSTATUS_S', '', '', 'vmicon vmicon-16-orderstatus', 3, 1, '', 'orderstatus', ''),
-(null, 5, 0, 'COM_VIRTUEMART_CURRENCY_S', '', '', 'vmicon vmicon-16-coins', 5, 1, '', 'currency', ''),
-(null, 5, 0, 'COM_VIRTUEMART_COUNTRY_S', '', '', 'vmicon vmicon-16-globe', 6, 1, '', 'country', ''),
-(null, 11, 0, 'COM_VIRTUEMART_MIGRATION_UPDATE', '', '', 'vmicon vmicon-16-installer_box', 1, 1, '', 'updatesmigration', ''),
-(null, 11, 0, 'COM_VIRTUEMART_ABOUT', '', '', 'vmicon vmicon-16-info', 2, 1, '', 'about', ''),
-(null, 11, 0, 'COM_VIRTUEMART_HELP_TOPICS', 'http://docs.virtuemart.net/', '', 'vmicon vmicon-16-help', 4, 1, '', '', ''),
-(null, 11, 0, 'COM_VIRTUEMART_COMMUNITY_FORUM', 'http://forum.virtuemart.net/', '', 'vmicon vmicon-16-reseller_programm', 6, 1, '', '', ''),
-(null, 11, 0, 'COM_VIRTUEMART_STATISTIC_SUMMARY', '', '', 'vmicon vmicon-16-info', 1, 1, '', 'virtuemart', ''),
-(null, 77, 0, 'COM_VIRTUEMART_USER_GROUP_S', '', '', 'vmicon vmicon-16-user', 2, 1, '', 'usergroups', '');
+INSERT INTO `#__virtuemart_adminmenuentries` ( `module_id`, `parent_id`, `name`, `link`, `depends`, `icon_class`, `ordering`, `published`, `tooltip`, `view`, `task`) VALUES
+( 1, 0, 'COM_VIRTUEMART_CATEGORY_S', '', '', 'icon-folder', 1, 1, '', 'category', ''),
+( 1, 0, 'COM_VIRTUEMART_PRODUCT_S', '', '', 'icon-camera', 2, 1, '', 'product', ''),
+( 1, 0, 'COM_VIRTUEMART_PRODUCT_CUSTOM_FIELD_S', '', '', 'icon-equalizer', 3, 1, '', 'custom', ''),
+( 1, 0, 'COM_VIRTUEMART_PRODUCT_INVENTORY', '', '', 'icon-health', 7, 1, '', 'inventory', ''),
+( 1, 0, 'COM_VIRTUEMART_CALC_S', '', '', 'icon-database', 8, 1, '', 'calc', ''),
+( 1, 0, 'COM_VIRTUEMART_REVIEW_RATE_S', '', '', 'icon-comments-2', 9, 1, '', 'ratings', ''),
+( 2, 0, 'COM_VIRTUEMART_ORDER_S', '', '', 'icon-stack', 1, 1, '', 'orders', ''),
+( 2, 0, 'COM_VIRTUEMART_COUPON_S', '', '', 'icon-scissors', 10, 1, '', 'coupon', ''),
+( 2, 0, 'COM_VIRTUEMART_REPORT', '', '', 'icon-bars', 3, 1, '', 'report', ''),
+( 2, 0, 'COM_VIRTUEMART_USER_S', '', '', 'icon-users', 4, 1, '', 'user', ''),
+( 2, 0, 'COM_VIRTUEMART_SHOPPERGROUP_S', '', '', 'icon-tree-2', 5, 1, '', 'shoppergroup', ''),
+( 3, 0, 'COM_VIRTUEMART_MANUFACTURER_S', '', '', 'icon-briefcase', 1, 1, '', 'manufacturer', ''),
+( 3, 0, 'COM_VIRTUEMART_MANUFACTURER_CATEGORY_S', '', '', 'icon-folder', 2, 1, '', 'manufacturercategories', ''),
+( 4, 0, 'COM_VIRTUEMART_STORE', '', '', 'icon-home-2', 1, 1, '', 'user', 'editshop'),
+( 4, 0, 'COM_VIRTUEMART_MEDIA_S', '', '', 'icon-pictures', 2, 1, '', 'media', ''),
+( 4, 0, 'COM_VIRTUEMART_SHIPMENTMETHOD_S', '', '', 'icon-box-add', 3, 1, '', 'shipmentmethod', ''),
+( 4, 0, 'COM_VIRTUEMART_PAYMENTMETHOD_S', '', '', 'icon-credit', 4, 1, '', 'paymentmethod', ''),
+( 5, 0, 'COM_VIRTUEMART_CONFIGURATION', '', '', 'icon-cogs', 1, 1, '', 'config', ''),
+( 5, 0, 'COM_VIRTUEMART_USERFIELD_S', '', '', 'icon-vcard', 2, 1, '', 'userfields', ''),
+( 5, 0, 'COM_VIRTUEMART_ORDERSTATUS_S', '', '', 'icon-upload', 3, 1, '', 'orderstatus', ''),
+( 5, 0, 'COM_VIRTUEMART_CURRENCY_S', '', '', 'icon-circle', 5, 1, '', 'currency', ''),
+( 5, 0, 'COM_VIRTUEMART_COUNTRY_S', '', '', 'icon-flag', 6, 1, '', 'country', ''),
+( 11, 0, 'COM_VIRTUEMART_MIGRATION_UPDATE', '', '', 'icon-unblock', 1, 1, '', 'updatesmigration', ''),
+( 11, 0, 'COM_VIRTUEMART_ABOUT', '', '', 'icon-info', 2, 1, '', 'about', ''),
+( 11, 0, 'COM_VIRTUEMART_HELP_TOPICS', 'http://virtuemart.net/', '', 'icon-help', 4, 1, '', '', ''),
+( 11, 0, 'COM_VIRTUEMART_COMMUNITY_FORUM', 'http://forum.virtuemart.net/', '', 'icon-users', 6, 1, '', '', ''),
+( 11, 0, 'COM_VIRTUEMART_STATISTIC_SUMMARY', '', '', 'icon-dashboard', 1, 1, '', 'virtuemart', ''),
+( 77, 0, 'COM_VIRTUEMART_USER_GROUP_S', '', '', 'vmicon vmicon-16-user', 2, 1, '', 'usergroups', ''),
+( 1, 0, 'COM_VIRTUEMART_MEDIA_S', '', '', 'icon-pictures', 4, 1, '', 'media&search_type=product', '');
 
 --
 -- Dumping data for table `#__virtuemart_modules`
@@ -62,12 +63,12 @@ INSERT INTO `#__virtuemart_modules` (`module_id`, `module_name`, `module_descrip
 --
 
 INSERT INTO `#__virtuemart_orderstates` (`virtuemart_orderstate_id`, `order_status_code`, `order_status_name`, `order_status_description`, `order_stock_handle`, `ordering`, `virtuemart_vendor_id`) VALUES
-(null, 'P', 'COM_VIRTUEMART_ORDER_STATUS_PENDING', '', 'R',1, 1),
-(null, 'U', 'COM_VIRTUEMART_ORDER_STATUS_CONFIRMED_BY_SHOPPER', '', 'R',2, 1),
-(null, 'C', 'COM_VIRTUEMART_ORDER_STATUS_CONFIRMED', '', 'R', 3, 1),
-(null, 'X', 'COM_VIRTUEMART_ORDER_STATUS_CANCELLED', '', 'A',4, 1),
-(null, 'R', 'COM_VIRTUEMART_ORDER_STATUS_REFUNDED', '', 'A',5, 1),
-(null, 'S', 'COM_VIRTUEMART_ORDER_STATUS_SHIPPED', '', 'O',6, 1);
+(null, 'P', 'Pending', '', 'R',1, 1),
+(null, 'U', 'Confirmed by shopper', '', 'R',2, 1),
+(null, 'C', 'Confirmed', '', 'R', 3, 1),
+(null, 'X', 'Cancelled', '', 'A',4, 1),
+(null, 'R', 'Refunded', '', 'A',5, 1),
+(null, 'S', 'Shipped', '', 'O',6, 1);
 
 
 --
@@ -81,7 +82,7 @@ INSERT INTO `#__virtuemart_userfields` (`virtuemart_userfield_id`, `virtuemart_v
 	(4, 0, 0, 'agreed', 'COM_VIRTUEMART_I_AGREE_TO_TOS', '', 'checkbox', NULL, NULL, 1, NULL, NULL, NULL, NULL, 1, 0, 1, 0, 0, 1, NULL, 13, 0, 1, '0000-00-00 00:00:00', 0, '0000-00-00 00:00:00', 0, '0000-00-00 00:00:00', 0),
 	(5, 0, 0, 'name', 'COM_VIRTUEMART_USER_DISPLAYED_NAME', '', 'text', 25, 30, 1, 0, 0, '', NULL, 1, 0, 1, 0, 0, 1, '', 8, 0, 1, '0000-00-00 00:00:00', 0, '0000-00-00 00:00:00', 0, '0000-00-00 00:00:00', 0),
 	(6, 0, 0, 'username', 'COM_VIRTUEMART_USERNAME', '', 'text', 25, 30, 1, 0, 0, '', NULL, 1, 0, 1, 0, 0, 1, '', 6, 0, 1, '0000-00-00 00:00:00', 0, '0000-00-00 00:00:00', 0, '0000-00-00 00:00:00', 0),
-	(7, 0, 0, 'address_type_name', 'COM_VIRTUEMART_USER_FORM_ADDRESS_LABEL', '', 'text', 32, 30, 1, NULL, NULL, NULL, 'Shipment', 0, 1, 0, 0, 0, 1, NULL, 16, 0, 1, '0000-00-00 00:00:00', 0, '0000-00-00 00:00:00', 0, '0000-00-00 00:00:00', 0),
+	(7, 0, 0, 'address_type_name', 'COM_VIRTUEMART_USER_FORM_ADDRESS_LABEL', '', 'text', 32, 30, 1, NULL, NULL, NULL, NULL, 0, 1, 0, 0, 0, 1, NULL, 16, 0, 1, '0000-00-00 00:00:00', 0, '0000-00-00 00:00:00', 0, '0000-00-00 00:00:00', 0),
 	(8, 0, 0, 'delimiter_billto', 'COM_VIRTUEMART_USER_FORM_BILLTO_LBL', '', 'delimiter', 25, 30, 0, NULL, NULL, NULL, NULL, 1, 0, 1, 0, 0, 0, NULL, 18, 0, 1, '0000-00-00 00:00:00', 0, '0000-00-00 00:00:00', 0, '0000-00-00 00:00:00', 0),
 	(9, 0, 0, 'company', 'COM_VIRTUEMART_SHOPPER_FORM_COMPANY_NAME', '', 'text', 64, 30, 0, NULL, NULL, NULL, NULL, 1, 1, 1, 0, 0, 1, NULL, 20, 0, 1, '0000-00-00 00:00:00', 0, '0000-00-00 00:00:00', 0, '0000-00-00 00:00:00', 0),
 	(10, 0, 0, 'title', 'COM_VIRTUEMART_SHOPPER_FORM_TITLE', '', 'select', 0, 0, 0, NULL, NULL, NULL, NULL, 1, 0, 1, 0, 0, 1, NULL, 22, 0, 1, '0000-00-00 00:00:00', 0, '0000-00-00 00:00:00', 0, '0000-00-00 00:00:00', 0),
